5392330923bd40515c4e17a56250ec44efb27806
Kameshwara Pavan Kumar Mantha <25398886+pavanjava@users.noreply.github.com>
2025-11-21 19:44:33 +0530

    code for finetuning ASR whisper (#13)

---
+++ stt/ft_whisper.py
@@ -0,0 +1,33 @@
+from datasets import load_dataset, DatasetDict, Audio
+from transformers import WhisperProcessor
+
+common_voice = DatasetDict()
+common_voice["train"] = load_dataset("Shekharmeena/Hindi-Female-Single-Speaker-High-Quality-Speech", split="train")
+
+split = common_voice["train"].train_test_split(test_size=0.2, seed=42)
+
+common_voice["train"] = split["train"]
+common_voice["test"] = split["test"]
+
+print(common_voice["train"])
+print(common_voice["test"])
+
+common_voice = common_voice.select_columns(["audio", "transcript"])
+
+processor = WhisperProcessor.from_pretrained(pretrained_model_name_or_path="openai/whisper-small", language="hi", task="transcribe")
+
+print(common_voice["train"].features)
+
+sampling_rate = processor.feature_extractor.sampling_rate
+print(sampling_rate)
+
+common_voice = common_voice.cast_column("audio", Audio(sampling_rate=sampling_rate, num_channels=1))
+print(common_voice["train"].features)
+
+def prepare_dataset(sample):
+    audio = sample["audio"]
+    sample = processor(audio["array"], sampling_rate=audio["sampling_rate"], transcript=sample["transcript"])
+    sample["input_length"]=len(audio["array"])/audio["sampling_rate"]
+    return sample
+
+common_voice = common_voice.map(prepare_dataset, remove_columns=common_voice.column_names["train"], num_proc=1)
